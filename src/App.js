46086f182db863d6dfad794ed0969e55aa29109c
Metasebia333 <metasebiagizawww@gmail.com>
2023-08-10 16:10:14 -0500

    Netflix-Clone

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Banner from './Banner';
+import Nav from './Nav';
+import requests from './requests';
+import Row from './Row';
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner/>
+      <Row title="NETFLIX ORIGINALS" 
+            fetchUrl={requests.fetchNetflixOriginals}
+            isLargeRow
+          />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
+			<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+			<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+			<Row
+				title="Documentaries Movies"
+				fetchUrl={requests.fetchDocumentaries}
+			/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
